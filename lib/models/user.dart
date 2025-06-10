e3a5538dd677fe0ae7f1d5b5a74cd9d02bb3c5ca
Herick <125935674+HerickYag@users.noreply.github.com>
2025-06-10 12:41:32 -0300

    Criar user.dart

---
+++ lib/models/user.dart
@@ -0,0 +1,32 @@
+// lib/models/user.dart
+class User {
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+  final String street;
+  final String city;
+  final String companyName;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.street,
+    required this.city,
+    required this.companyName,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      username: json['username'],
+      email: json['email'],
+      street: json['address']['street'],
+      city: json['address']['city'],
+      companyName: json['company']['name'],
+    );
+  }
+}
